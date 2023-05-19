f468e24994860b581592647cb3d6e07fc2dfcb02
Jeremy Novak <olepunchy@gmail.com>
2023-05-19 11:18:29 -0500

    Frontend and Backend source buttons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,15 +8,18 @@ const date = new Date().getFullYear();
 const Footer = () => {
     return (
         <footer className="mx-auto max-w-6xl">
-            <div className="hidden md:flex p-4 text-zinc-900 items-center justify-between">
+            <div className="hidden md:flex p-4 mx-4 mt-8 text-white items-center justify-between bg-slate-800 rounded-md shadow-lg">
                 <div className="items-center justify-between">
                     &copy; Jeremy Novak {date}
                 </div>
-                <ButtonGitHub url="https://github.com/jgnovakdev/loanshark"/>
+                <div className="flex md:space-x-2 lg:space-x-8">
+                    <ButtonGitHub url="https://github.com/jgndev/loanshark" text="Frontend Source"/>
+                    <ButtonGitHub url="https://github.com/jgndev/loanshark-api" text="Backend Source"/>
+                </div>
                 <Socials/>
             </div>
-            <div className="block md:hidden text-zinc-900">
-                <div className="flex mb-2 p-4 items-center justify-between">
+            <div className="block md:hidden">
+                <div className="flex m-4 p-4 items-center justify-between text-slate-200 bg-slate-800 rounded-md shadow-lg">
                     &copy; Jeremy Novak {date}
                     <Socials/>
                 </div>
